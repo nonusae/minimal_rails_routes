1ad34b78ed1f16f0b4f90485a64c46e0b27db1df
nonusae <nonusae@gmail.com>
2021-04-27 17:38:08 +0700

    Initialize project and create base classes

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,15 +1,21 @@
-class Configuration
-  def inititialize(*)
-    @config = {}
+class RouteSet
+  def initialize(attribute)
+    # Minimal version, for real produciton ready we version we may have to use ordered list
+    @routes = {}
+  end
 
-    attributes.each do |attributes|
-      define_singleton_method attribute do
-        @config[attributes]
-      end
+  def draw(&block)
+  end
+end
 
-      define_singleton_method ":#{attribute}=" do |value|
-        @config[attributes] = value
-      end
-    end
+class RouteMapper
+  def initialize(rotue_set)
+    @rotue_set = rotue_set
+  end
+end
+
+class Rails
+  def self.routes
+    @routes ||= RouteSet.new
   end
 end
